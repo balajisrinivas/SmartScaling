ceea64d53117580528e01c0e138b7185b9093abb
balajisrinivas <balaji.srinivasan@stonybrook.edu>
2014-11-09 03:24:12 -0500

    Added dataGenerator.py, input.txt and output.txt

---
+++ src/dataGenerator.py
@@ -0,0 +1,55 @@
+import random
+from random import seed
+
+if __name__ == '__main__':
+    inputFileName = "input.txt"
+    outputFileName = "output.txt"
+    f1 = open(inputFileName,'r')
+    f2 = open(outputFileName,"r+")
+    stockSymbolList = []
+    dataList=[]
+    for line in f1:
+        line = line.rstrip('\n')
+        if line.startswith("date"):
+            flag1 = 1
+        elif line.startswith("stock_symbols"):
+            flag2 = 1
+            flag1 = 0
+        elif line == '':
+            pass
+        elif flag1 == 1:
+            date=int(line)
+        else:
+            stockSymbolList.append(line)
+    #print date
+    #print stockSymbolList
+    numHours = 12
+    numMinutes = 24
+    numSeconds = 24
+    numMilliseconds = 1000
+    lenStockSymbolList = len(stockSymbolList)
+    random.seed(1000)
+    for i in range(numHours):
+        for j in range(numMinutes):
+            for k in range(numSeconds):
+                for l in range(numMilliseconds):
+                    tdata=""
+                    tdata+=str(date)+str(i).zfill(2)+str(j).zfill(2)+str(k).zfill(2)+"."+str(l).zfill(3)
+                    #print tdata
+                    tdata += ','
+                    stockSymbolListIndex = random.randrange(0,lenStockSymbolList,1)
+                    tdata += stockSymbolList[stockSymbolListIndex]
+                    tdata += ','
+                    open = random.uniform(30,100)
+                    low = open - random.uniform(0,20)
+                    high = open + random.uniform(0,20)
+                    if(random.random() < 0.5):
+                        close = (low + open)/2
+                    else:
+                        close = (open + high)/2
+                    tdata += str(open)+','+str(high)+','+str(low)+','+str(close)
+                    tdata +='\n'
+                    print tdata
+                    f2.write(tdata)
+    f1.close()
+    f2.close()
