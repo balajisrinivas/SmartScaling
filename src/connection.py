0bb1e5877746e54cefec5440659da081403f7cc0
mkrish7 <msomanurrama@cs.stonybrook.edu>
2014-11-14 02:19:21 -0500

    Added batch insert and response time

--- src/connection.py
+++ src/connection.py
@@ -1,7 +1,9 @@
 from cassandra.cluster import Cluster
 from cassandra import ConsistencyLevel
 from cassandra.query import SimpleStatement
+from cassandra.query import BatchStatement
 
+import time
 import logging
 
 log = logging.getLogger()
@@ -39,12 +41,18 @@ class SimpleClient:
 	self.session.execute(create_column_family)
 	print("Column family created.")
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    def load_data(self, keyspace, tableName):
+    def load_data(self, keyspace, tableName, number):
 
 	out_file = open('output.txt','r')
 	data = out_file.readlines()
 
-	for line in data:				
+	insertQuery = self.session.prepare("""INSERT INTO """+keyspace+"""."""+tableName+""" (id, company, open, high, low, close) VALUES (?,?,?,?,?,?) ;""")
+	batch = BatchStatement(consistency_level=ConsistencyLevel.ANY)
+
+	counter = 0
+	for line in data:
+		counter = counter + 1
+				
 		t_date, t_company, t_openVal, t_highVal, t_lowVal, t_closeVal = line.split(",")
 
 		s_date = str(t_date)
@@ -57,27 +65,48 @@ class SimpleClient:
 		global idVal
 		idVal = s_date
 
- 		print("Inserting : "+idVal+", "+company+", "+openVal+", "+highVal+", "+lowVal+", "+closeVal)
-      		insertQuery = self.session.prepare("""INSERT INTO """+keyspace+"""."""+tableName+""" (id, company, open, high, low, close) VALUES (?,?,?,?,?,?) ;""")
-		insertQuery.consistency_level = ConsistencyLevel.ANY
-		self.session.execute(insertQuery, (idVal, company, openVal, highVal, lowVal, closeVal))
-	
+		batch.add(insertQuery, (idVal, company, openVal, highVal, lowVal, closeVal))
+
+		if counter == number:
+			print("Inserting batch of "+str(counter)+" elements.")
+
+			start = time.time()
+			self.session.execute(batch)
+			end = time.time()
+
+			# Send the response time to monitor server.
+			print("Response time : "+str(end - start)+ " seconds.")
+			counter = 0
+
+
+	if counter > 0 :
+		print("Inserting  last batch of "+str(counter)+" elements.")
+		start = time.time()
+		self.session.execute(batch)
+		end = time.time()
+
+		# Send the response time to monitor server.
+		print("Response time : "+str(end - start)+ " seconds.")
+
 	out_file.close()
         print('Data loaded.')
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def query_schema(self, keyspace, tableName):
 
+	start = time.time()
         results = self.session.execute("""
-	    SELECT * FROM """+keyspace+"""."""+tableName+""" WHERE id = '"""+idVal+"""' ;""") # currently fetch last record in db.
-
+	    SELECT * FROM """+keyspace+"""."""+tableName+""" ;""")
+	end = time.time()
         print("Data  retrived :")
 	print("")
 
+	print "%-30s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\n%s" % ("id", "Company", "OpenValue", "HighValue", "LowValue", "CloseValue", "-------------------------------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------")
+
         for row in results:
-	    print "%-30s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\n%s" % ("id", "Company", "OpenValue", "HighValue", "LowValue", "CloseValue", "-------------------------------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------")
             print "%-30s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s" % (row.id, row.company, row.open, row.high, row.low, row.close)
 	
 	print("")
+	print("Response time : "+str(end - start)+" seconds.")
         print('Schema queried.')
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
